db33a4f8bed4c6d156599551614b0c6ed1c328c4
sailboat0116 <dennis04020116@gmail.com>
2024-10-30 01:23:42 +0800

    1029-1

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sqlite3 = require('sqlite3').verbose();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,20 +18,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var db = new sqlite3.Database(path.join(__dirname, 'db', 'course.db'), (err) => {
+  if (err) {
+    console.error('資料庫連接失敗:', err.message);
+  } else {
+    console.log('已成功連接到 SQLite3 資料庫。');
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 登錄路由
+app.post('/api/courses', (req, res) => {
+  let { id } = req.body;
+  let sql = 'SELECT * FROM course WHERE id = ?';
+  db.all(sql, [id], (err, row) => {
+    if (err) {
+      return res.status(500).send({ message: '資料庫錯誤' });
+    }
+    if (row) {
+      res.json(row[0]);
+    } else {
+      res.send({ success: false, message: '無效的憑證' });
+    }
+  });
 });
 
 module.exports = app;
